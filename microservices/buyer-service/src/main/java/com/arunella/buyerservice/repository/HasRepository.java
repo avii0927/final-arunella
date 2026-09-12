10bf081836ae783532e1d6f74c24be09f0ae9988
aviii0927 <avishkasenanayaka02@gmail.com>
2026-09-12 14:29:31 +0530

    code fix

--- microservices/buyer-service/src/main/java/com/arunella/buyerservice/repository/HasRepository.java
+++ microservices/buyer-service/src/main/java/com/arunella/buyerservice/repository/HasRepository.java
@@ -2,13 +2,12 @@ package com.arunella.buyerservice.repository;
 
 import com.arunella.buyerservice.entity.Has;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface HasRepository extends JpaRepository<Has, Long> {
     List<Has> findByOrderId(Long orderId);
+
     List<Has> findByProductId(Long productId);
+
     void deleteByOrderIdAndProductId(Long orderId, Long productId);
 }
