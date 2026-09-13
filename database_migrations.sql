b94dcc887358f0954989c878b2a77e16261d07ab
aviii0927 <avishkasenanayaka02@gmail.com>
2026-09-13 15:46:13 +0530

    docker

--- database_migrations.sql
+++
@@ -1,8 +0,0 @@
--- Migration script to remove obsolete columns from arunella_db database
-USE arunella_db;
-
--- 1. Remove `image` column from `crop` table
-ALTER TABLE `crop` DROP COLUMN IF EXISTS `image`;
-
--- 2. Remove `wallet` column from `farmer` table
-ALTER TABLE `farmer` DROP COLUMN IF EXISTS `wallet`;
--- drop_crop_image_column.sql
+++
@@ -1,4 +0,0 @@
--- Migration script to delete the `image` column from `crop` table in database
-USE arunella_db;
-
-ALTER TABLE `crop` DROP COLUMN IF EXISTS `image`;
